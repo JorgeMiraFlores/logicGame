b8453a9015094a625ad62cf8ab3e26776ed7144f
JorgeMiraFlores <jorge.mira@oportunidades.org.sv>
2024-03-05 16:14:33 -0600

    Logic has been done

--- mostrarInfo.php
+++ mostrarInfo.php
@@ -8,5 +8,10 @@
 
     
     echo "<h1>El arca de Noé entran muchas cosas. ¿Qué cosas entran?</h1>";
-    echo ": " . $palabra;
+    if(contains($palabra, "e")){
+        echo "La palabra: " . $palabra . " no entra en el arca de Noé";
+    }else{
+        echo "La palabra: " . $palabra . " entra en el arca de Noé";
+    }
+
 ?>
