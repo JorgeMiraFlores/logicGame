2bad06cacf43d40eebd282e88b6c5f8257d3247a
JorgeMiraFlores <jorge.mira@oportunidades.org.sv>
2024-03-05 16:25:36 -0600

    Letter identifier has been added

--- mostrarInfo.php
+++ mostrarInfo.php
@@ -8,7 +8,7 @@
 
     
     echo "<h1>El arca de Noé entran muchas cosas. ¿Qué cosas entran?</h1>";
-    if(contains($palabra, "e")){
+    if(str_contains($palabra, "e" || "E")){
         echo "La palabra: " . $palabra . " no entra en el arca de Noé";
     }else{
         echo "La palabra: " . $palabra . " entra en el arca de Noé";
